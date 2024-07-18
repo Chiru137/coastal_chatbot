8cc5cd759524908a7bc3ae0730150177698e5ba0
Chiru137 <164709609+Chiru137@users.noreply.github.com>
2024-07-18 21:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -224,7 +224,6 @@ def weather():
     return render_template('weather.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
